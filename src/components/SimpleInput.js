994fd68ec1bef4d78b320da897a545dceac1be50
Danushka Pathirana <pathirana.danushka.rp@gmail.com>
2023-02-13 02:00:18 +0530

    develop getting user input

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState('')
+  const nameInputRef = useRef()
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value)
+  }
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault()
+    console.log(enteredName)
+
+    const enteredNameValue = nameInputRef.current.value
+    console.log(enteredNameValue)
+
+    // nameInputRef.current.value = '' //don't do this, not ideal, do not manipulate the real DOM
+    setEnteredName('')
+  }
+
   return(
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" onChange={nameInputChangeHandler} ref={nameInputRef} value={enteredName} />
       </div>
 
       <div className="form-actions">
