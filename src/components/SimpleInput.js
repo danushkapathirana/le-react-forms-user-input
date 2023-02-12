027e67b54ccfafb37ff07a75224709531af4e000
Danushka Pathirana <pathirana.danushka.rp@gmail.com>
2023-02-13 02:47:34 +0530

    refactor the code

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,28 +1,19 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState('')
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false) //putting initial state is true is not good (even if it is for only to apply styles)
   const [enteredNameTouched, setEnteredNameTouched] = useState(false) //use to check whether user was tried to enter something or not
-  const nameInputRef = useRef()
+
+  const enteredNameIsValid = enteredName.trim() !== ''
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value)
-
-    // disappear the error msg (if it is displaying)
-    if(event.target.value.trim() !== '') {
-      setEnteredNameIsValid(true)
-    }
   }
 
   // validate when input is lose focus (when user focus the input field and leave it as empty)
   const nameInputBlurHandler = (event) => {
     setEnteredNameTouched(true)
-
-    if(enteredName.trim() === '') {
-      setEnteredNameIsValid(false)
-      return
-    }
   }
 
   const formSubmissionHandler = (event) => {
@@ -30,23 +21,15 @@ const SimpleInput = (props) => {
 
     setEnteredNameTouched(true)
 
-    if(enteredName.trim() === '') {
-      setEnteredNameIsValid(false)
+    if(!enteredNameIsValid) {
       return
     }
 
-    setEnteredNameIsValid(true)
-
     console.log(enteredName)
 
-    const enteredNameValue = nameInputRef.current.value
-    console.log(enteredNameValue)
-
-    // nameInputRef.current.value = '' //don't do this, not ideal, do not manipulate the real DOM
     setEnteredName('')
+    setEnteredNameTouched(false)
   }
-
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched
 
   const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control'
 
@@ -54,7 +37,7 @@ const SimpleInput = (props) => {
     <form onSubmit={formSubmissionHandler}>
       <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" onChange={nameInputChangeHandler} onBlur={nameInputBlurHandler} ref={nameInputRef} value={enteredName} />
+        <input type="text" id="name" onChange={nameInputChangeHandler} onBlur={nameInputBlurHandler} value={enteredName} />
         {nameInputIsInvalid && <p className="error-text">Name must not be empty.</p>}
       </div>
 
